01b14955beda8d1c3b82c9e7c0a1d1e4e38a38ec
CPQi <CPQi@CPQi-PC>
2016-05-26 08:10:43 -0300

    MASTER-Acesso web

---
+++ war-feliz/src/main/java/webapp/JUtil.java
@@ -0,0 +1,13 @@
+package webapp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class JUtil
+{ 
+
+	public static String dataFeliz()
+	{
+		return new SimpleDateFormat("hh:mm:ss").format(new Date());
+	}
+
+}
